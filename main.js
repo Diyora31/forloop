14684d8c5fcf1cea502dbed89e927b3ac6714eff
Diyora31 <asilovad7>
2024-05-10 02:10:25 +0500

    done

---
+++ main.js
@@ -0,0 +1,52 @@
+// let arr = ['behruz', 'dilbek', 'diyora', 'amirbek', 'uyqudagi malika']
+// let res2 = []
+
+// for (let text of res) {
+//     let res = (text.split(""))
+//     let res3 = (res.reverse(''))
+//     res2.push(res3.join(''))
+// }
+
+// console.log(res2);
+
+
+
+
+
+
+// // let arr = ['behruz', 'dilbek', 'diyora', 'amirbek', 'uyqudegi malika']
+// // let resArr = []
+// // for (let element of arr) {
+// //     let res = element.split("")
+// //     let res1 = res.reverse()
+// //     resArr.push(res1.join(''))
+
+
+
+// //     // console.log(resArr);
+
+// // }
+// // console.log(resArr);
+
+
+
+
+
+let arr=['non', 'asal','banan', 'olma','aziza' ,'gul','mashina']
+
+let res1=[]
+
+let res2=[]
+
+
+for(let word of arr){
+if (word .includes('n')){
+    res1.push(word) 
+}
+
+else{
+    res2.push(word)
+}
+}
+
+console.log(res1 ,res2);
